524a7d88b380703dbe0250c2519e2a1a628e9073
sudo.developer <erfanmashari2@gmail.com>
2023-03-03 23:59:29 +0330

    Change project page styles

--- src/views/Project/index.vue
+++ src/views/Project/index.vue
@@ -60,46 +60,46 @@
     <transition name="modal">
         <popUp v-if="createNewTask" @close="createNewTask = false">
             <p class="font-bold my-3">تسک جدید ایجاد کنید:</p>
-            <div class="flex gap-4">
-                <div class="mb-3 w-1/2">
+            <div class="flex flex-col sm:flex-row gap-2 sm:gap-4 mb-3">
+                <div class="w-full sm:w-1/2">
                     <p class="mb-2">نام تسک:</p>
-                    <InputText v-model="taskName" type="text" placeholder="نام تسک..." class="w-full" />
+                    <InputText v-model="taskName" type="text" placeholder="نام تسک..." class="w-full rounded-lg py-1.5 px-3" />
                 </div>
-                <div class="w-1/2">
+                <div class="w-full sm:w-1/2">
                     <p class="mb-2">پروژه مربوط:</p>
                     <p class="mt-3">{{ currentProject.name }}</p>
                 </div>
             </div>
-            <div class="flex gap-2 items-center mb-4">
-                <div class="w-1/4">
+            <div class="flex items-center flex-shrink flex-wrap mb-4">
+                <div class="w-2/4 sm:w-1/4 flex flex-col justify-center items-center mb-2 sm:mb-0">
                     <p class="mb-2">فرد مسئول:</p>
                     <Dropdown v-model="selectedDropTeammate" :options="projectTeammateDrop" optionLabel="name"
                         placeholder="همکار" class="drop-down" />
                 </div>
-                <div class="w-1/4">
+                <div class="w-2/4 sm:w-1/4 flex flex-col justify-center items-center mb-2 sm:mb-0">
                     <template v-if="selectedDropTeammate && Object.values(selectedDropTeammate).length > 0">
                         <p class="mb-2">امتیاز تسک:</p>
-                        <InputNumber v-model="selectedPoint" showButtons dir="ltr" inputClass="w-16" :min="0" />
+                        <InputNumber v-model="selectedPoint" showButtons dir="ltr" inputClass="w-16" :min="0" class="rounded-lg overflow-hidden" />
                     </template>
                 </div>
-                <div class="w-1/4">
+                <div class="w-2/4 sm:w-1/4 flex flex-col justify-center items-center mb-2 sm:mb-0">
                     <template v-if="selectedPoint !== 0">
                         <p class="mb-2">دوره ددلاین:</p>
                         <Dropdown v-model="selectedDropDeadlinePeriod" :options="deadlinePeriodDrop" optionLabel="name"
                             placeholder="دوره" class="drop-down" />
                     </template>
                 </div>
-                <div class="w-1/4">
+                <div class="w-2/4 sm:w-1/4 flex flex-col justify-center items-center mb-2 sm:mb-0">
                     <template
                         v-if="selectedDropDeadlinePeriod && Object.values(selectedDropDeadlinePeriod).length > 0 && selectedPoint !== 0">
                         <p class="mb-2">واحد دوره:</p>
-                        <InputNumber v-model="selectedUnit" showButtons dir="ltr" inputClass="w-16" :min="0" />
+                        <InputNumber v-model="selectedUnit" showButtons dir="ltr" inputClass="w-16" :min="0" class="rounded-lg overflow-hidden" />
                     </template>
                 </div>
             </div>
             <div class="mb-5">
                 <p class="mb-2">توضیحات:</p>
-                <Editor v-model="taskDescription" editorStyle="height: 150px" dir="ltr">
+                <Editor v-model="taskDescription" editorStyle="height: 150px" dir="ltr" class="rounded-xl overflow-hidden">
                     <template #toolbar>
                         <span class="ql-formats">
                             <button class="ql-bold"></button>
@@ -117,11 +117,11 @@
                     </template>
                 </Editor>
             </div>
-            <div class="flex gap-2">
-                <Button label="انصراف" class="p-button-sm p-button-danger" @click="createNewTask = false" />
-                <Button label="ایجاد" class="p-button-sm p-button-success"
+            <div class="w-full flex justify-center items-center gap-2">
+                <Button label="ایجاد" class="p-button-sm p-button-success w-20 h-10 rounded-lg"
                     :disabled="!(taskName.length > 0 && selectedPoint !== 0 && selectedDropTeammate && selectedDropDeadlinePeriod && selectedUnit !== 0)"
                     @click="addTask" />
+                <Button label="انصراف" class="p-button-sm p-button-danger w-20 h-10 rounded-lg" @click="createNewTask = false" />
             </div>
         </popUp>
     </transition>
